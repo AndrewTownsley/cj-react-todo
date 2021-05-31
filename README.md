7a76586ec4f131dcd504a82e7b692d87d2ff2481
Andrew TOwnsley <atownsley86@gmail.com>
2021-05-30 21:13:18 -0600

    todo app done

--- README.md
+++ README.md
@@ -1,13 +1,13 @@
-[] 1. create-react-app
-[] 2. Create new Todo form.
-[] 3. Call a functio when the form is submitted.
-[] 4. Create a string state value to store the user input.
-[] 5. Listen for when the input changes and update the state.
-[] 6. Log the user input when the form is submitted.
-[] 7. Create an array state value for todos.
-[] 8. Add a new todo into the todos array, with done:false.
-[] 9. Show the new todos in a list.
-[] 10. Check done on a todo to mark it as done.
+[x] 1. create-react-app
+[x] 2. Create new Todo form.
+[x] 3. Call a function when the form is submitted.
+[x] 4. Create a string state value to store the user input.
+[x] 5. Listen for when the input changes and update the state.
+[x] 6. Log the user input when the form is submitted.
+[x] 7. Create an array state value for todos.
+[x] 8. Add a new todo into the todos array, with done:false.
+[x] 9. Show the new todos in a list.
+[x] 10. Check done on a todo to mark it as done.
 [] 10.1 Show a line through the completed todo
 [] 11. Add a button to remove a todo.
 [] 12. Add a button to mark all todos as done.
--- src/App.js
+++ src/App.js
@@ -1,12 +1,110 @@
-import React from 'react';
+import React, { useState, useCallback, useEffect } from 'react';
+import "./App.css";
 
 function App() {
+  // hooks...
+  const [newTodo, setNewTodo] = useState('');
+  const [todos, setTodos] = useState([]);
+
+  const onNewTodoChange = useCallback((event) => {
+    setNewTodo(event.target.value);
+  }, []);
+
+  const formSubmitted = useCallback((event) => {
+    event.preventDefault();
+    if(!newTodo.trim()) return;
+    setTodos([// the spread operator[...todos] takes the existing todos array and adds in the newly created todo.
+      
+      {
+        id: todos.length ? todos[0].id + 1: 1,
+        content: newTodo,
+        done: false,
+      },
+      ...todos,// adding the spread operator here adds the newt todo to the top
+    ]);
+
+    
+
+    setNewTodo('');
+  }, [newTodo,todos]);
+
+  useEffect(() => {
+    // console.log("todos",todos);
+  }, [todos]);
+
+  const addTodo = useCallback((todo, index) => (event) => {
+      const newTodos = [...todos];
+      newTodos.splice(index, 1, {
+        ...todo,
+        done: !todo.done
+      });
+      setTodos(newTodos);
+    }, [todos]);
+
+    const deleteTodo = useCallback((todo) => (event) =>{
+      setTodos(todos.filter(otherTodo => otherTodo !== todo));
+    }, [todos])
   
+    const markAllDone = useCallback(() => {
+      // Create a copy of the array
+      // create a copy of each of the items
+      // Update the done property to be "true" on each of the items.
+      const updatedTodos = todos.map((todo) => {
+        return {
+          ...todo,
+          done: true,
+        }
+      })
+      setTodos(updatedTodos)
+    }, [todos])
+
   return (
     <div>
-     <h1>heloo world</h1>
+      <form onSubmit={formSubmitted}>
+        <label htmlFor="newTodo" >Enter Todo</label>
+        <input 
+          value={newTodo} 
+          onChange={onNewTodoChange} 
+          id="newTodo"/>
+        <button>Add Todo</button>
+      </form>
+      <button onClick={markAllDone}>Mark all as done</button>
+      <ul>
+        {todos.map((todo, index) => {
+          return <li key={todo.id}>
+            <input 
+              checked={todo.done}
+              type="checkbox"
+              onChange={addTodo(todo, index)} 
+            />
+            
+            <span className={todo.done ? 'done' : ''}>
+              {todo.content}
+            </span>
+            <button onClick={deleteTodo(todo)}>Delete</button>
+            </li>
+        })
+        }
+      </ul>
     </div>
     );
 }
 
 export default App;
+
+// const [name, setName] = useState("AnDrEw");
+// const onNameChange = useCallback((event) => {
+//   console.log(event.target.value);
+//   setName(event.target.value);
+// }, []) 
+
+
+/* <form action="">
+<label>Enter your Name</label>
+<input 
+  value={name} 
+  onChange={onNameChange}
+// onChange={(event) => {console.log(event) setName(event.target.value);}}
+/>
+</form>
+<h1>Hello {name}</h1> */
